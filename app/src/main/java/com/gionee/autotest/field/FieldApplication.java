b2ec337bc84beb51862e3db8e229cd86fb4935af
viking den <dengwj@gionee.com>
2017-11-07 13:49:33 +0800

    add FieldApplication

---
+++ app/src/main/java/com/gionee/autotest/field/FieldApplication.java
@@ -0,0 +1,21 @@
+package com.gionee.autotest.field;
+
+import android.app.Application;
+
+import com.gionee.autotest.common.Preference;
+import com.gionee.autotest.field.util.Constant;
+
+/**
+ * Created by viking on 11/7/17.
+ *
+ * Application main entry point
+ */
+public class FieldApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //init preference
+        Preference.initName(Constant.PREF_NAME);
+    }
+}
