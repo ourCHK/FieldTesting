2d9fc5f1768b973613ada0a47c5058e496183227
xionghk <xionghk@gionee.com>
2017-11-15 10:35:07 +0800

    增加数据重激活UI

---
+++ app/src/main/java/com/gionee/autotest/field/ui/data_reset/DataResetContract.java
@@ -0,0 +1,18 @@
+package com.gionee.autotest.field.ui.data_reset;
+
+import com.gionee.autotest.field.ui.base.BaseView;
+
+/**
+ * Created by xhk on 2017/11/15.
+ */
+
+public interface DataResetContract {
+
+    interface View extends BaseView {
+
+    }
+
+    interface Presenter {
+
+    }
+}
