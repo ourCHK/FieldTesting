63eb58963d93b1bbc510a526b5cf4763c0c094a8
xionghk <xionghk@gionee.com>
2017-11-15 17:43:24 +0800

    add message ui

---
+++ app/src/main/java/com/gionee/autotest/field/ui/message/MessageActivity.java
@@ -0,0 +1,50 @@
+package com.gionee.autotest.field.ui.message;
+
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.gionee.autotest.field.R;
+import com.gionee.autotest.field.ui.base.BaseActivity;
+
+/**
+ * Created by xhk on 2017/11/15.
+ */
+
+public class MessageActivity extends BaseActivity {
+
+
+    @Override
+    protected int layoutResId() {
+        return R.layout.activity_message;
+    }
+
+    @Override
+    protected void initializePresenter() {
+
+    }
+
+    @Override
+    protected boolean isDisplayHomeUpEnabled() {
+        return true;
+    }
+
+    @Override
+    protected int menuResId() {
+        return R.menu.menu_main;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+                Toast.makeText(getApplicationContext(), R.string.not_implemented_yet, Toast.LENGTH_SHORT).show();
+                //                startActivity(new Intent(this, AboutActivity.class));
+                return true;
+            case R.id.help:
+                Toast.makeText(getApplicationContext(), R.string.not_implemented_yet, Toast.LENGTH_SHORT).show();
+                //                startActivity(new Intent(this, AboutActivity.class));
+                return true ;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
