80e1b6a770022a73fef68723efbbcfe9fcc2dd7a
songyc <songyc@gionee.com>
2017-11-15 18:03:11 +0800

    新增InComingDBManager

---
+++ app/src/main/java/com/gionee/autotest/field/data/db/InComingDBManager.java
@@ -0,0 +1,45 @@
+package com.gionee.autotest.field.data.db;
+
+
+import android.content.Context;
+
+import com.gionee.autotest.common.call.CallMonitorParam;
+import com.gionee.autotest.common.call.CallMonitorResult;
+import com.gionee.autotest.field.data.db.model.InComingReportBean;
+
+import java.util.ArrayList;
+
+public class InComingDBManager {
+
+    private static InComingDBHelper helper;
+
+    static void init(Context context) {
+        if (helper == null) {
+            helper = new InComingDBHelper(context);
+        }
+    }
+
+    public static long addBatch(CallMonitorParam param) {
+       return helper.addBatch(param);
+    }
+
+    public static void writeData(CallMonitorResult data) {
+        helper.writeData(data);
+    }
+
+    public static void delete() {
+        helper.clearTable();
+    }
+
+    public static InComingReportBean getReportBean(int batchId){
+      return  helper.getReportBean(batchId);
+    }
+
+    public static ArrayList<String> getAllBatch(){
+      return  helper.getAllBatch();
+    }
+
+    public static int getLastBatch(){
+        return helper.getLastBatch();
+    }
+}
